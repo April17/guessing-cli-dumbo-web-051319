6e24a7a55fe8023d036e77a9de5b5b13739099e5
Fanqiang Meng <fanmeng0417@gmail.com>
2019-05-03 21:48:26 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -2,7 +2,7 @@ require "pry"
 def run_guessing_game
   input = ""
   ranNumber = rand(1..6)
-  for i in 0..5
+  for i in 0..1
     puts "Guess a number between 1 and 6."
     input = "exit"
     binding.pry
