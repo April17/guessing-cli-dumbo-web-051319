a1b67d0cb42bb8e83d8e1fb3ce1fc34101716538
Fanqiang Meng <fanmeng0417@gmail.com>
2019-05-03 21:46:09 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,10 +1,11 @@
 require "pry"
 def run_guessing_game
   input = ""
+  ranNumber = rand(1..6)
   for i in 0..5
     puts "Guess a number between 1 and 6."
     input = ""
-    ranNumber = rand(1..6)
+
     input = gets.downcase.strip
     if input.to_i == ranNumber
       puts "You guessed the correct number!"
