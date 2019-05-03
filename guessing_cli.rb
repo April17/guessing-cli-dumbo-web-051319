e5b8afb54e332c43f126c1fb29cd60d3a348d02a
Fanqiang Meng <fanmeng0417@gmail.com>
2019-05-03 21:43:09 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,1 +1,20 @@
-# Code your solution here!
+require "pry"
+def run_guessing_game
+  input = ""
+  while input
+    input = ""
+    ranNumber = rand(1..6)
+    input = gets.downcase.strip
+    binding.pry
+    if input.to_i == ranNumber
+      puts "You guessed the correct number!"
+    elsif input.to_i != ranNumber
+      puts "The computer guessed #{ranNumber}."
+    elsif input == "exit"
+      "Goodbye!"
+      break
+    else
+      nil
+    end
+  end
+end
